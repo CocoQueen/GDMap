cbed04c0d117384b5d08f5271aefee522db6fb5d
Coco <13522975595@163.com>
2020-10-30 09:40:10 +0800

    添加呼吸点

--- app/src/main/java/com/example/gdmap/MainActivity.java
+++ app/src/main/java/com/example/gdmap/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,6 +34,10 @@ import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.amap.api.maps.model.Polyline;
 import com.amap.api.maps.model.PolylineOptions;
+import com.amap.api.maps.model.animation.AlphaAnimation;
+import com.amap.api.maps.model.animation.Animation;
+import com.amap.api.maps.model.animation.AnimationSet;
+import com.amap.api.maps.model.animation.ScaleAnimation;
 import com.amap.api.maps.utils.SpatialRelationUtil;
 import com.amap.api.maps.utils.overlay.MovingPointOverlay;
 import com.example.gdmap.permiss.PermissionsUtils;
@@ -43,7 +48,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements LocationSource {
+public class MainActivity extends AppCompatActivity implements LocationSource, AMapLocationListener {
 
     private MapView mMapView;
     MyLocationStyle myLocationStyle;
@@ -54,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.ACCESS_FINE_LOCATION};
     private AMapLocation privLocation;
+    private Marker noLineMaker;
 
 
     @Override
@@ -79,14 +85,49 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
             aMap = mMapView.getMap();
             myLocationStyle = new MyLocationStyle();
             myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+            // 设置圆形的边框颜色
             myLocationStyle.strokeColor(Color.argb(50, 30, 150, 180));
+            // 设置圆形的填充颜色
             myLocationStyle.radiusFillColor(Color.argb(50, 30, 150, 180));
+            // 设置圆形的边框粗细
             myLocationStyle.strokeWidth(1.0f);
+            //设置显示定位按钮 并且可以点击
             UiSettings settings = aMap.getUiSettings();
+            //设置了定位的监听
             aMap.setLocationSource(this);
+            // 是否显示定位按钮
             settings.setMyLocationButtonEnabled(true);
+            //显示定位层并且可以触发定位,默认是flase
             aMap.setMyLocationEnabled(true);
+            // 定位、且将视角移动到地图中心点,定位点依照设备方向旋转,并且会跟随设备移动。
         }
+        //开始定位
+        location();
+    }
+
+    private void location() {
+        //初始化定位
+        mlocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mlocationClient.setLocationListener(this);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为Hight_Accuracy高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        mLocationOption.setOnceLocation(false);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //给定位客户端对象设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mlocationClient.startLocation();
     }
 
     @Override
@@ -144,6 +185,7 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
     private Polyline mPolyline;
 
     private void addPolylinesWithColors() {
+        //用一个数组来存放颜色，四个点对应三段颜色
         List<Integer> colorList = new ArrayList<Integer>();
 
         PolylineOptions options = new PolylineOptions();
@@ -167,6 +209,11 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
                 .colorValues(colorList).useGradient(true));
     }
 
+    /**
+     * 读取轨迹点
+     *
+     * @return
+     */
     private List<LatLng> readLatLngs() {
         List<LatLng> points = new ArrayList<>();
         for (int i = 0; i < coords.length; i += 2) {
@@ -210,6 +257,7 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
             return;
         }
         List<LatLng> points = readLatLngs();
+        // 构建 轨迹的显示区域
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
         builder.include(points.get(0));
         builder.include(points.get(points.size() - 2));
@@ -248,7 +296,6 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
                         @Override
                         public void run() {
                             if (infoWindowLayout != null && title != null) {
-                                Log.e("=============", "run: " + distance);
                                 title.setText("距离终点还有： " + (int) distance + "米");
                             }
                         }
@@ -316,6 +363,11 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
         return infoWindowLayout;
     }
 
+    /**
+     * 添加闪烁的标记点
+     *
+     * @param view
+     */
     public void addMarker(View view) {
         setUpMap();
     }
@@ -329,17 +381,104 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
         normalGiflist.add(BitmapDescriptorFactory.fromResource(R.drawable.icon_normal38));
 
         for (int i = 0; i < coords.length; i += 2) {
-            Marker marker = aMap.addMarker(new MarkerOptions().position(new LatLng(coords[i + 1], coords[i])).anchor(0.5f, 0.5f).icons(normalGiflist).period(3));
+            Marker marker = aMap.addMarker(new MarkerOptions().position(new LatLng(coords[i + 1], coords[i])).anchor(0.5f, 0.5f).icons(normalGiflist).period(15));
             markerList.add(marker);
         }
     }
 
+    /**
+     * 移除所有添加的点标记
+     *
+     * @param view
+     */
     public void removeMarker(View view) {
-        for (Marker marker :markerList) {
+        for (Marker marker : markerList) {
             marker.remove();
         }
-//        aMap.clear();
-//        addPolylinesWithColors();
-//        startMove();
+    }
+
+    /**
+     * 添加呼吸点
+     *
+     * @param view
+     */
+    public void breathMarker(View view) {
+        //呼吸点
+        noLineMaker = aMap.addMarker(new MarkerOptions().position(new LatLng(39.97693511, 116.34892166))
+                .zIndex(1).anchor(0.5f, 0.5f).icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_no_line1)));
+        //呼吸点中心点
+        aMap.addMarker(new MarkerOptions().position(new LatLng(39.97693511, 116.34892166)).zIndex(2)
+                .anchor(0.5f, 0.5f).icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_no_line1)));
+
+        AnimationSet animationSet = new AnimationSet(true);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.5f, 0f);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setRepeatCount(Animation.INFINITE);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1, 3.5f, 1, 3.5f);
+        scaleAnimation.setDuration(2000);
+        scaleAnimation.setRepeatCount(Animation.INFINITE);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.setInterpolator(new LinearInterpolator());
+        noLineMaker.setAnimation(animationSet);
+        noLineMaker.startAnimation();
+    }
+
+    private boolean isFirstLoc = true;
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (mListener != null && aMapLocation != null) {
+            if (aMapLocation != null && aMapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
+                aMapLocation.getLatitude();//获取纬度
+                aMapLocation.getLongitude();//获取经度
+                aMapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                aMapLocation.getCountry();//国家信息
+                aMapLocation.getProvince();//省信息
+                aMapLocation.getCity();//城市信息
+                aMapLocation.getDistrict();//城区信息
+                aMapLocation.getStreet();//街道信息
+                aMapLocation.getStreetNum();//街道门牌号信息
+                aMapLocation.getCityCode();//城市编码
+                aMapLocation.getAdCode();//地区编码
+
+                // 如果不设置标志位，此时再拖动地图时，它会不断将地图移动到当前的位置
+                if (isFirstLoc) {
+                    //设置缩放级别
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+                    //将地图移动到定位点
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                    //点击定位按钮 能够将地图的中心移动到定位点
+                    mListener.onLocationChanged(aMapLocation);
+                    //添加图钉
+                    // aMap.addMarker(getMarkerOptions(amapLocation));
+                    //获取定位信息
+                    StringBuffer buffer = new StringBuffer();
+                    buffer.append(aMapLocation.getCountry() + ""
+                            + aMapLocation.getProvince() + ""
+                            + aMapLocation.getCity() + ""
+                            + aMapLocation.getProvince() + ""
+                            + aMapLocation.getDistrict() + ""
+                            + aMapLocation.getStreet() + ""
+                            + aMapLocation.getStreetNum());
+                    Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
+                    isFirstLoc = false;
+                    // 显示系统小蓝点
+                    mListener.onLocationChanged(aMapLocation);
+                }
+            } else {
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
